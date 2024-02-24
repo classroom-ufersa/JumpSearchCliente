bff4e885c9285d4f3c7edc91bd21011a464df4df
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-02-24 15:41:01 -0300

    problema das funcoes resolvidos

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {  
         ContarAlunos(nomedoarquivo,&QuantidadeDeClientes);
-        Cliente * usuario = (Cliente*)malloc(QuantidadeDeClientes * sizeof(usuario));
+        Cliente * usuario = (Cliente*)malloc(QuantidadeDeClientes * sizeof(Cliente));
         PassarDados(nomedoarquivo,&usuario,&QuantidadeDeClientes);
         
         printf("====MENU====\n\n");
